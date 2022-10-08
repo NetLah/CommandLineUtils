2ed1735126bdc00cee3ce9922a93ffcbe4172b09
Tho Ho <code@tho.email>
2022-10-08 22:45:21 +0800

    Refactoring; use file-scoped namespace

--- CommandLineUtils/CommandLine/CommandParsingException.cs
+++ CommandLineUtils/CommandLine/CommandParsingException.cs
@@ -1,19 +1,17 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
 using System;
 
-namespace NetLah.Extensions.CommandLineUtils
-{
-    public class CommandParsingException : Exception
-    {
-        public CommandParsingException(CommandLineApplication command, string message)
-            : base(message)
-        {
-            Command = command;
-        }
+namespace NetLah.Extensions.CommandLineUtils;
 
-        public CommandLineApplication Command { get; }
+public sealed class CommandParsingException : Exception
+{
+    public CommandParsingException(CommandLineApplication command, string message)
+        : base(message)
+    {
+        Command = command;
     }
+
+    public CommandLineApplication Command { get; }
 }
